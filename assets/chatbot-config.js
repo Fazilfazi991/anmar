53289a16554140daaf74c53bef28c0eb21fc05b9
Fazilfazi991 <fazilfazi991@gmail.com>
2026-06-07 19:20:47 +0400

    Connect leads endpoint

--- assets/chatbot-config.js
+++ assets/chatbot-config.js
@@ -1,5 +1,5 @@
 window.ANMAR_CHATBOT_CONFIG = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbxgIlAqPjkuEFmLh1ff41kaRU3C6PLcgQzv4D-Ci75JZXYDBoRXORGVWIcoOTkN75xW/exec",
   notifyEmail: "operations@almrebid.sa",
   whatsapp: "966598626402"
 };
